6385fcff88898fd6ef89ceea57b3390cec68a8b0
JeffreyEarly <jeffrey@jeffreyearly.com>
2024-07-12 15:57:44 -0700

    Update create_along_track_db.py

--- create_along_track_db.py
+++ create_along_track_db.py
@@ -3,6 +3,7 @@ from AlongTrack import AlongTrackDatabase
 atdb = AlongTrackDatabase("localhost", "postgres")
 atdb.create_database()
 atdb.create_along_track_table()
+atdb.create_along_track_indices()
 # atdb.create_ocean_basin_connection_tables()
 # atdb.create_along_track_table_partitions('monthly')
 # atdb.insert_along_track_data_from_netcdf('path/to/file.nc')
