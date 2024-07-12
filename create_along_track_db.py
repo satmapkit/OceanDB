996f50023258edec50e1a212fe39a0ec6d588e36
JeffreyEarly <jeffrey@jeffreyearly.com>
2024-07-12 08:16:50 -0700

    Adding basin masks, updating readme

--- create_along_track_db.py
+++ create_along_track_db.py
@@ -1,27 +1,5 @@
-import psycopg2
-from psycopg2 import sql
+from AlongTrack import AlongTrackDatabase
 
-# Connection details
-host = "localhost"
-user = "postgres"
-port = "5432"  # typically 5432 for PostgreSQL
-
-# Connect to the default database
-conn = psycopg2.connect(dbname="postgres", user=user, host=host, port=port)
-conn.autocommit = True  # Enable autocommit to execute CREATE DATABASE command
-
-# Create a cursor object
-cur = conn.cursor()
-
-# Define the new database name
-new_database_name = "along-track"
-
-# Create the new database
-cur.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(new_database_name)))
-cur.execute(sql.SQL("CREATE EXTENSION postgis;"))
-            
-# Close the cursor and connection
-cur.close()
-conn.close()
-
-print(f"Database '{new_database_name}' created successfully.")
+atdb = AlongTrackDatabase("localhost", "postgres")
+atdb.create_database()
+atdb.create_along_track_table()
