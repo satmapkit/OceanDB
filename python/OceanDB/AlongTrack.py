6d9e9f16944ad2a9d2dcfc5f277604a1808c7eae
Jeffrey J. Early <JeffreyEarly@users.noreply.github.com>
2024-07-29 22:53:32 -0700

    Update AlongTrack.py

--- python/OceanDB/AlongTrack.py
+++ python/OceanDB/AlongTrack.py
@@ -141,24 +141,16 @@ class AlongTrack:
 
     def create_along_track_indices(self):
         tokenized_query = self.sql_query_with_name('create_along_track_index_point.sql')
-        query_create_point_index = sql.SQL(tokenized_query).format(
-            table_name=sql.Identifier(self.along_track_table_name)
-        )
+        query_create_point_index = sql.SQL(tokenized_query).format(table_name=sql.Identifier(self.along_track_table_name))
 
         tokenized_query = self.sql_query_with_name('create_along_track_index_date.sql')
-        query_create_date_index = sql.SQL(tokenized_query).format(
-            table_name=sql.Identifier(self.along_track_table_name)
-        )
+        query_create_date_index = sql.SQL(tokenized_query).format(table_name=sql.Identifier(self.along_track_table_name))
 
         tokenized_query = self.sql_query_with_name('create_along_track_index_filename.sql')
-        query_create_filename_index = sql.SQL(tokenized_query).format(
-            table_name=sql.Identifier(self.along_track_table_name)
-        )
+        query_create_filename_index = sql.SQL(tokenized_query).format(table_name=sql.Identifier(self.along_track_table_name))
 
         tokenized_query = self.sql_query_with_name('create_along_track_index_point_date.sql')
-        query_create_combined_point_date_index = sql.SQL(tokenized_query).format(
-            table_name=sql.Identifier(self.along_track_table_name)
-        )
+        query_create_combined_point_date_index = sql.SQL(tokenized_query).format(table_name=sql.Identifier(self.along_track_table_name))
 
         with pg.connect(self.connect_string()) as conn:
             with conn.cursor() as cur:
