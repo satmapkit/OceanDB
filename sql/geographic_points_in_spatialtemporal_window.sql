3197dfbd46bdc1b36ecde69d23b81f0610808724
Nazanne <bcurtis@mac.com>
2024-08-01 14:24:12 -0700

    Update spatial/temporal query for geography types

--- sql/geographic_points_in_spatialtemporal_window.sql
+++ sql/geographic_points_in_spatialtemporal_window.sql
@@ -3,15 +3,15 @@ SELECT
      latitude,
 	 sla_filtered,
 	 date_time,
-	 ST_DistanceSphere(ST_GeogFromText('POINT({longitude} {latitude})')),along_track_point) as dist
+	 ST_Distance(ST_GeogFromText('POINT({longitude} {latitude})'),along_track_point) as dist
 FROM along_track alt
-LEFT JOIN basin on ST_Contains(basin.basin_geom, alt.along_track_point)
-WHERE ST_DWithin(ST_GeogFromText('POINT({longitude} {latitude})'),along_track_point::geography, 100000)
+LEFT JOIN basin on ST_Intersects(basin.basin_geog, alt.along_track_point)
+WHERE ST_DWithin(ST_GeogFromText('POINT({longitude} {latitude})'),along_track_point, 100000)
 	AND date_time::date BETWEEN {min_date} AND {max_date}
 	AND basin.id IN (
 	 	 SELECT DISTINCT
 	 	 	 UNNEST(ARRAY[pbc.connected_id, basin_id])
 		FROM basin pb
 		LEFT JOIN basin_connection pbc on basin_id = pb.id
-		WHERE ST_Contains(pb.basin_geog, ST_GeogFromText('POINT({longitude} {latitude})'))
+		WHERE ST_Intersects(pb.basin_geog, ST_GeogFromText('POINT({longitude} {latitude})'))
 	);
