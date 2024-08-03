ca8b5e8668d9c3bb632e9ee610d93fbac22c5838
Jeffrey J. Early <JeffreyEarly@users.noreply.github.com>
2024-08-03 07:42:20 -0700

    New spatial temporal query, following best (better?) practices

--- sql/geographic_points_in_spatialtemporal_window.sql
+++ sql/geographic_points_in_spatialtemporal_window.sql
@@ -3,15 +3,16 @@ SELECT
      latitude,
 	 sla_filtered,
 	 date_time,
-	 ST_Distance(ST_GeogFromText('POINT({longitude} {latitude})'),along_track_point) as dist
+	 ST_Distance(ST_MakePoint(%(longitude)s, %(latitude)s),along_track_point) as dist,
+	 EXTRACT(EPOCH FROM (%(central_date_time)s - date_time)) AS time_difference_secs
 FROM along_track alt
 LEFT JOIN basin on ST_Intersects(basin.basin_geog, alt.along_track_point)
-WHERE ST_DWithin(ST_GeogFromText('POINT({longitude} {latitude})'),along_track_point, {distance})
-	AND date_time::date BETWEEN {min_date} AND {max_date}
+WHERE ST_DWithin(ST_MakePoint(%(longitude)s, %(latitude)s),along_track_point, %(distance)s)
+	AND date_time BETWEEN %(central_date_time)s - %(time_delta)s AND %(central_date_time)s + %(time_delta)s
 	AND basin.id IN (
 	 	 SELECT DISTINCT
 	 	 	 UNNEST(ARRAY[pbc.connected_id, basin_id])
 		FROM basin pb
 		LEFT JOIN basin_connection pbc on basin_id = pb.id
-		WHERE ST_Intersects(pb.basin_geog, ST_GeogFromText('POINT({longitude} {latitude})'))
+		WHERE ST_Intersects(pb.basin_geog, ST_MakePoint(%(longitude)s, %(latitude)s))
 	);
--- sql/geographic_points_in_spatialtemporal_window_nomask_alt.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-	 longitude,
-     latitude,
-	 sla_filtered,
-	 date_time,
-	 ST_DistanceSphere(ST_SetSRID(ST_MakePoint({longitude}, {latitude}),4326),along_track_point) as dist
-FROM along_track alt
-WHERE ST_Contains(ST_Envelope(ST_Buffer(ST_GeogFromText('POINT({longitude} {latitude})'),{distance})::geometry), along_track_point)
-	AND date_time::date BETWEEN {min_date} AND {max_date};
