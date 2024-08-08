81b0dfdac12c04986896d7fe4db7345a5229e263
Cimarron Wortham <worthamc@gmail.com>
2024-08-08 16:31:48 -0700

    Update copernicus_download_netcdf_data.py

--- python/old-scripts/copernicus_download_netcdf_data.py
+++ python/old-scripts/copernicus_download_netcdf_data.py
@@ -12,8 +12,7 @@ import yaml
 with open('../config.yaml', 'r') as param_file:
     along_params = yaml.full_load(param_file)
 
-# missions = ['tp', 'j1', 'j2', 'j3', 's3a', 's3b', 's6a-lr']
-missions = ['s6a-lr']
+missions = ['tp', 'j1', 'j2', 'j3', 's3a', 's3b', 's6a-lr']
 
 my = {'al': '202112',
       'alg': '202112',
