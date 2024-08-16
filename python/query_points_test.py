bb109aad499720281f9940ecb6ec9d82633d75af
Cimarron Wortham <worthamc@gmail.com>
2024-08-15 18:40:15 -0700

    Cleanup query_points_test.py

--- python/query_points_test.py
+++ python/query_points_test.py
@@ -10,26 +10,21 @@ atdb = AlongTrack(db_name='ocean')
 # data = atdb.geographic_points_in_spatialtemporal_window(11, -80, 800000, '2002-05-15', '2002-05-25', should_basin_mask=0)
 # data = atdb.geographic_points_in_spatialtemporal_window(11, 150, 800000, '2002-05-15', '2002-05-25', should_basin_mask=0)
 
-data = atdb.geographic_points_in_spatialtemporal_window(11, 150, datetime.datetime(2002,5,15))
-
+data = atdb.geographic_points_in_spatialtemporal_window(11, 150, datetime.datetime(2022,5,15))
 
 aa = np.array(data)
 
-x = [data_i[0] for data_i in data]
-y = [data_i[1] for data_i in data]
-sla = [data_i[2] for data_i in data]
+x = np.array([data_i[0] for data_i in data])
+y = np.array([data_i[1] for data_i in data])
+sla = np.array([data_i[2] for data_i in data])
+t = np.array([data_i[3] for data_i in data])
 
 plt.figure()
 plt.scatter(x, y, c=sla)
 plt.show()
-#
-# x = np.array(x)
-# y = np.array(y)
-# sla = np.array(sla)
-#
-# np.stack((x, y), axis=1)
 
-[x, y, sla] = atdb.projected_points_in_spatialtemporal_window(11, 150, datetime.datetime(2002,5,15))
+
+[x, y, sla, t] = atdb.projected_points_in_spatialtemporal_window(11, 150, datetime.datetime(2022,5,15))
 plt.figure()
 plt.scatter(x, y, c=sla)
 plt.show()
