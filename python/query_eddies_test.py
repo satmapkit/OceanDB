eebcd71f49520ea0a39a8b6d3f259fc2d943f8b7
Nazanne <bcurtis@mac.com>
2024-08-20 15:25:00 -0700

    Initial sample eddy query

---
+++ python/query_eddies_test.py
@@ -0,0 +1,25 @@
+import json
+
+from OceanDB.Eddy import Eddy
+import folium
+from folium import GeoJson
+import webbrowser
+import os
+import psycopg as pg
+from psycopg import sql
+
+eddy_db = Eddy(db_name='ocean')
+
+eddy_track = 41
+cyclonic_type = -1
+
+speed_radii = eddy_db.eddy_speed_radii_json(eddy_track, cyclonic_type)
+
+map = folium.Map(
+width="%100",
+height="%100",
+zoom_start=10)
+GeoJson(speed_radii[0][0]).add_to(map)
+
+map.save('test.html')
+webbrowser.open('file://'+ os.getcwd() + '/test.html', new=2)
