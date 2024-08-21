79e43e14a4e43d77ff424e22148e81d5bcd1c72a
Nazanne <bcurtis@mac.com>
2024-08-21 11:14:53 -0700

    Add zoom to eddies test

--- python/query_eddies_test.py
+++ python/query_eddies_test.py
@@ -14,15 +14,21 @@ eddy_track = 41
 cyclonic_type = -1
 
 speed_radii = eddy_db.eddy_speed_radii_json(eddy_track, cyclonic_type)
-print(speed_radii[0][1])
-print(speed_radii[0][2])
-print(speed_radii[0][3])
-print(speed_radii[0][4])
+max_latitude = speed_radii[0][1]
+min_latitude = speed_radii[0][2]
+max_longitude = speed_radii[0][3]
+min_longitude = speed_radii[0][4]
+sw_corner = [min_longitude, min_latitude-360]
+ne_corner = [max_longitude, max_latitude-360]
 
 map = folium.Map(
-width="%100",
-height="%100",
-zoom_start=10)
+    width="%100",
+    height="%100",
+    max_bounds=True,
+    no_wrap = True,
+)
+print(sw_corner)
+map.fit_bounds([ne_corner, sw_corner])
 GeoJson(speed_radii[0][0]).add_to(map)
 
 map.save('test.html')
