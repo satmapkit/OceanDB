acb7dc83ec3419f9aa9ccc71c500d5a48586367f
Daniel Neshyba-Rowe <rowedanielp@gmail.com>
2026-02-04 23:25:59 -0800

    Fix typo with schema keys being used to index database fields instead of OceanDataField.name

--- src/OceanDB/data_access/base_query.py
+++ src/OceanDB/data_access/base_query.py
@@ -165,10 +165,10 @@ class BaseQuery(OceanDB):
         row0 = rows[0]
 
         for name, field in schema.items():
-            if name not in row0:
+            if field.name not in row0:
                 continue
 
-            values = [row[name] for row in rows]
+            values = [row[field.name] for row in rows]
 
             if field.python_type is not None:
                 arr = np.asarray(values, dtype=field.python_type)
