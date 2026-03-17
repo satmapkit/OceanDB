b72ce6b286dbd3a50679ebede36a852d1a23084a
Daniel Neshyba-Rowe <rowedanielp@gmail.com>
2026-03-17 13:58:36 -0700

    Test basin_connection's creation

--- tests/database/test_ingest.py
+++ tests/database/test_ingest.py
@@ -5,23 +5,28 @@ from OceanDB.etl import EddyETL
 
 from .fixtures import *
 
+
 def test_insert_basin_data(db_with_basin_data):
     with pg.connect(db_with_basin_data.connection_string) as conn:
         with conn.cursor() as cur:
             cur.execute("SELECT COUNT(*) FROM basin;")
             res = cur.fetchone()
             assert res
-            # should have at least one entry in basin tabl
-            assert res[0] > 0 
-    
+            # should have at least one entry in basin table
+            assert res[0] > 0
+
+            cur.execute("SELECT COUNT(*) FROM basin_connections;")
+            res = cur.fetchone()
+            assert res
+            # should have at least one entry in basin table
+            assert res[0] > 0
+
 
 def test_ingest_eddy(db_with_basin_data):
     oceandb_etl = EddyETL(config=db_with_basin_data.config)
     eddy_directory = oceandb_etl.config.eddy_data_directory
 
-    cyclonic_filepath = Path(
-        f"{eddy_directory}/cyclonic.nc"
-    )
+    cyclonic_filepath = Path(f"{eddy_directory}/cyclonic.nc")
     oceandb_etl.ingest_eddy_data_file(cyclonic_filepath, cyclonic_type=-1)
 
     # print(
