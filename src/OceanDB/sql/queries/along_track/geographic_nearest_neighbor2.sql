f6199e01ed58977d7a0fe9066a9ff27fc7110020
Daniel Neshyba-Rowe <rowedanielp@gmail.com>
2026-07-20 19:16:03 -0700

    Make nearest neighbor have optional max radius

--- src/OceanDB/sql/queries/along_track/geographic_nearest_neighbor2.sql
+++ src/OceanDB/sql/queries/along_track/geographic_nearest_neighbor2.sql
@@ -1,20 +1,11 @@
-SELECT {fields} distance
-FROM (
-    SELECT q.*
-    FROM unnest(%(connected_basin_ids)s) AS basins(basin_id)
-    CROSS JOIN LATERAL (
-        SELECT
-            latitude,
-            longitude,
-            along_track_point <-> ST_SetSRID(ST_MakePoint(%(longitude)s, %(latitude)s), 4326) AS distance
-        FROM along_track atk
-        WHERE date_time BETWEEN %(central_date_time)s - %(time_delta)s::interval
-                            AND %(central_date_time)s + %(time_delta)s::interval
-          AND mission = ANY(%(missions)s)
-          AND basin_id = basins.basin_id
-        ORDER BY along_track_point <-> ST_SetSRID(ST_MakePoint(%(longitude)s, %(latitude)s), 4326)
-        LIMIT 3
-
-    ) AS q
-) AS atk
-ORDER BY distance LIMIT 3;
+SELECT
+    {fields}
+    atk.along_track_point <-> ST_SetSRID(ST_MakePoint(%(longitude)s, %(latitude)s), 4326) AS distance
+FROM along_track atk
+WHERE date_time BETWEEN %(central_date_time)s - %(time_delta)s::interval
+                    AND %(central_date_time)s + %(time_delta)s::interval
+  AND basin_id = ANY(%(connected_basin_ids)s)
+  AND mission = ANY(%(missions)s)
+  AND atk.along_track_point <-> ST_SetSRID(ST_MakePoint(%(longitude)s, %(latitude)s), 4326) <= %(max_radius)s
+ORDER BY distance
+LIMIT 3;
---
+++ src/OceanDB/sql/queries/nearest2.sql
@@ -0,0 +1,20 @@
+SELECT {fields}, distance
+FROM (
+    SELECT q.*
+    FROM (%(connected_basin_ids)s) AS basins(basin_id)
+    CROSS JOIN LATERAL (
+        SELECT
+            latitude,
+            longitude,
+            along_track_point <-> ST_SetSRID(ST_MakePoint(%(longitude)s, %(latitude)s), 4326) AS distance
+        FROM along_track atk
+        WHERE date_time BETWEEN %(central_date_time)s - %(time_delta)s::interval
+                            AND %(central_date_time)s + %(time_delta)s::interval
+          AND mission = ANY(%(missions)s)
+          AND basin_id = basins.basin_id
+        ORDER BY along_track_point <-> ST_SetSRID(ST_MakePoint(%(longitude)s, %(latitude)s), 4326) AS distance
+        LIMIT 3
+
+    ) AS q
+) AS candidates
+ORDER BY distance LIMIT 3;
