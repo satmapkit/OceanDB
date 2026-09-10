26aabe29b18e708253bb54b2382540302c086e66
Daniel Neshyba-Rowe <rowedanielp@gmail.com>
2026-09-10 12:30:40 -0700

    Plot index performance from JSON results

--- graph_index_performance.py
+++ graph_index_performance.py
@@ -1,17 +1,11 @@
-import pickle
+import json
 import matplotlib.pyplot as plt
 
 
-filename = "geo_points_in_r_dt.pickle"
-title = "Compound index (all 4) + 4 singleton indexes, -60 to 60 lat"
-with open(filename, "rb") as file:
-    data = pickle.load(file)
-nodes = data["tried_nodes"]
-
-# filename = "no_mission_singleton_indexes.pickle"
-# title = "Compound index without mission + 4 singleton indexes, -60 to 60 lat"
-# with open(filename, "rb") as file:
-#     nodes = pickle.load(file)
+filename = "no_mission_singleton_indexes.json"
+title = "Compound index without mission + 4 singleton indexes, -60 to 60 lat"
+with open(filename, encoding="utf-8") as file:
+    nodes = json.load(file)
 
 field_names_short = {
         "mission": "m",
@@ -29,16 +23,24 @@ scenario_names = [
 performance = {name: [] for name in scenario_names}
 index_names = []
 for node in nodes:
-    index_names.append("".join(field_names_short[field] for field in node.fields))
+    index_name = (
+        "" if not node["trial_indexes"] else node["trial_indexes"][0]["name"]
+    )
+    for field, short_name in field_names_short.items():
+        index_name = index_name.replace(field, short_name)
+    index_name = index_name.removeprefix("along_track_index_experiment_").replace(
+        "_", ""
+    )
+    index_names.append(index_name)
     print(index_names[-1])
 
-    if node.performance is None:
+    if node["performance"] is None:
         for name in scenario_names:
             performance[name].append(float('nan'))
         continue
-    for scenario_i, res in enumerate(node.performance):
+    for scenario_i, res in enumerate(node["performance"]):
         name = scenario_names[scenario_i]
-        performance[name].append(res.total_time)
+        performance[name].append(res["total_time"])
 
 fig, ax = plt.subplots(layout='constrained')
 
